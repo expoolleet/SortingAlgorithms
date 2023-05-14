6b0160bae42166f73bcd1b5cbcb114a76ae594d3
Георгий Роуч <gosha.shcherbak@mail.ru>
2023-05-14 11:36:59 +0300

    Добавьте файлы проекта.

---
+++ SortingAlgorithms/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using SortingAlgorithms.Algorithms;
+
+namespace SortingAlgorithms
+{
+    internal class Program
+    {
+        private static Stopwatch _sw = new Stopwatch();
+
+        static void Main()
+        {
+            List<int> lengthList = new List<int> { 1000, 2000, 3000, 4000};
+
+            double timeWork;
+            double[] sortTime = { 0, 0, 0, 0, 0, 0, 0 };
+
+            foreach (var currentLength in lengthList)
+            {
+                Console.WriteLine($"----------     Количество элементов массива {currentLength}    ----------");
+
+                int[] array = new int[currentLength];
+                int[] newArray = new int[currentLength];
+
+                FillArray(array);
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.StupidSort, newArray, out timeWork);
+                Console.WriteLine($"1. Время работы алгоритма Тупая Сортировка: {timeWork}");
+                sortTime[0] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.BubbleSort, newArray, out timeWork);
+                Console.WriteLine($"2. Время работы алгоритма Пузырьковая Сортировка: {timeWork}");
+                sortTime[1] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.CocktailSort, newArray, out timeWork);
+                Console.WriteLine($"3. Время работы алгоритма Коктейльная Сортировка: {timeWork}");
+                sortTime[2] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.InsertionSort, newArray, out timeWork);
+                Console.WriteLine($"4. Время работы алгоритма Сортировки Простыми Вставками: {timeWork}");
+                sortTime[3] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.GnomeSort, newArray, out timeWork);
+                Console.WriteLine($"5. Время работы алгоритма Гномья Сортировка: {timeWork}");
+                sortTime[4] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.SelectionSort, newArray, out timeWork);
+                Console.WriteLine($"6. Время работы алгоритма Сортировка Выбором: {timeWork}");
+                sortTime[5] += timeWork;
+
+                Array.Copy(array, newArray, array.Length);
+                StartTest(Sorting.ShellSort, newArray, out timeWork);
+                Console.WriteLine($"7. Время работы алгоритма Сортировка Шелла: {timeWork}");
+                sortTime[6] += timeWork;
+
+                Console.WriteLine("\n\n");
+            }
+            Console.WriteLine("Общее время работы каждого алгоритма по всем длиннам массива");
+            for (int i = 0; i < sortTime.Length; i++)
+            {
+                Console.WriteLine($"{i+1}: {sortTime[i]/sortTime.Length}");
+            }
+
+            Console.Write("y/n to continue/close console... ");
+
+            switch (Console.ReadLine())
+            {
+                case "y": Main(); break;
+                case "n": Environment.Exit(0); break;
+            }
+        }
+
+        private static void StartTest(Action<int[]> action, int[] array, out double timeWork)
+        {
+            timeWork = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                _sw.Reset();
+
+                _sw.Start();
+
+                action(array);
+
+                _sw.Stop();
+
+                timeWork += _sw.ElapsedMilliseconds;
+            }
+
+            timeWork = timeWork / 10;
+        }
+
+        private static void FillArray(int[] array)
+        {
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+                array[i] = rand.Next(-array.Length, array.Length);
+        }
+    }
+}
---
+++ SortingAlgorithms/Sorting.cs
@@ -0,0 +1,137 @@
+﻿namespace SortingAlgorithms
+{
+    internal class Sorting
+    {
+        public static void StupidSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                if (array[i] > array[i + 1])
+                {
+                    var temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    i = 0;
+                }
+            }
+        }
+
+        public static void BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        var temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        public static void CocktailSort(int[] array)
+        {
+            int right = array.Length;
+            int left = 0;
+
+            while (left <= right)
+            {
+                for (int i = left; i < right - 1; i++)
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        var temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+                left++;
+                for (int i = right - left - 1; i > left; i--)
+                {
+                    if (array[i] < array[i - 1])
+                    {
+                        var temp = array[i];
+                        array[i] = array[i - 1];
+                        array[i - 1] = temp;
+                    }
+                }
+                right--;
+            }
+        }
+
+        public static void InsertionSort(int[] array)
+        {
+            for (int i = 1; i < array.Length; i++)
+            {
+                var key = array[i];
+                int j = i - 1;
+                while (j >= 0 && key < array[j])
+                {
+                    array[j + 1] = array[j];
+                    j--;
+                }
+                array[j + 1] = key;
+            }
+        }
+
+        public static void GnomeSort(int[] array)
+        {
+            int i = 0;
+            while (i < array.Length)
+            {
+                if (i == 0 || array[i] >= array[i - 1])
+                {
+                    i++;
+                }
+                else
+                {
+                    var temp = array[i];
+                    array[i] = array[i - 1];
+                    array[i - 1] = temp;
+                    i--;
+                }
+            }
+        }
+
+        public static void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min_index = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[min_index] > array[j])
+                    {
+                        min_index = j;
+                    }
+                }
+                var temp = array[min_index];
+                array[min_index] = array[i];
+                array[i] = temp;
+            }
+        }
+
+        public static void ShellSort(int[] array)
+        {
+            int d = array.Length;
+            while(d > 0)
+            {
+                for (int i = d; i < array.Length; i++)
+                {
+                    var key = array[i];
+                    int j = i;
+                    while (j >= d && key < array[j - d])
+                    {
+                        array[j] = array[j - d];
+                        j -= d;
+                    }
+                    array[j] = key;
+                }
+                d /= 2;
+            }          
+        }
+    }
+}
